d7958551594ef44db7216b3d940a055643e029af
Kirill <hotasice41@gmail.com>
2019-11-15 15:13:24 +0100

    откат изменений

--- Lesson_1/9.py
+++ Lesson_1/9.py
@@ -1,18 +1,3 @@
 # 9.Вводятся три разных числа. Найти, какое из них
 # является средним (больше одного, но меньше другого).
 
-NUM1 = int(input("Введите первое число:"))
-NUM2 = int(input("Введите второе число:"))
-NUM3 = int(input("Введите третье число:"))
-
-if ((NUM1 > NUM2) and (NUM1 < NUM3)) or ((NUM1 < NUM2) and (NUM1 > NUM3)):
-    print(f"Среднее число {NUM1}")
-
-elif ((NUM2 > NUM1) and (NUM2 < NUM3)) or ((NUM2 < NUM1) and (NUM2 > NUM3)):
-    print(f"Среднее число {NUM2}")
-
-elif ((NUM3 > NUM2) and (NUM3 < NUM1)) or ((NUM3 < NUM2) and (NUM3 > NUM1)):
-    print(f"Среднее число {NUM3}")
-
-else:
-    print("Пожалуйста, введите разные числа!")
