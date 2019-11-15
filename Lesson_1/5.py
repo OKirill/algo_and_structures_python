750132052304d89e1a4239342a2bdc825f546d53
Kirill <hotasice41@gmail.com>
2019-11-15 14:42:11 +0100

    Задание  5 готово

--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,2 +1,14 @@
 #5.	Пользователь вводит две буквы. Определить, на каких местах
 # алфавита они стоят, и сколько между ними находится букв.
+
+LETTER1 = ord(input("Введите первую Букву: ").lower())
+LETTER2 = ord(input("Введите вторую Букву: ").lower())
+
+print(LETTER1 - 96)
+print(LETTER2 - 96)
+
+if LETTER1 > LETTER2:
+    print(f"Букв между ними: {LETTER1 - LETTER2 - 1}")
+
+else:
+    print(f"Букв между ними: {LETTER2 - LETTER1 - 1}")
