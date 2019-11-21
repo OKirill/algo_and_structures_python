cc01c23764948714f3745ca37c0b6fad7cb52b0e
Kirill <hotasice41@gmail.com>
2019-11-21 13:13:23 +0100

    9 задание

--- Lesson_3/9.py
+++ Lesson_3/9.py
@@ -1,1 +1,24 @@
 # 9.	Найти максимальный элемент среди минимальных элементов столбцов матрицы.
+from random import random
+
+M = int(input("Введите кол-во столбцов матрицы: "))
+N = int(input("Введите кол-во строк матрицы: "))
+arr = []
+for i in range(N):
+    b = []
+    for j in range(M):
+        n = int(random() * 1000)
+        b.append(n)
+        print(f"{n:5d}", end='')
+    arr.append(b)
+    print()
+
+maximum = -1
+for j in range(M):
+    minimum = 1000
+    for i in range(N):
+        if arr[i][j] < minimum:
+            minimum = arr[i][j]
+    if minimum > maximum:
+        maximum = minimum
+print("Максимальный элемент среди минимальных: ", maximum)
