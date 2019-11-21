358f59c4020385097429e023fb935bcee91c9dd3
Kirill <hotasice41@gmail.com>
2019-11-21 12:03:54 +0100

    4 задание

--- Lesson_3/4.py
+++ Lesson_3/4.py
@@ -1,1 +1,22 @@
 # 4.	Определить, какое число в массиве встречается чаще всего.
+from random import random
+
+N = int(input("Укажите длину массива: "))
+ARR = [0] * N
+for i in range(N):
+    f = True
+    ARR[i] = int(random() * 10)
+    print(f"|{ARR[i]:3d}|", end=' ')
+
+
+MNUMB = 0
+MCOUNT = 0
+
+for i in ARR:
+    if ARR.count(i) > MCOUNT:
+        MNUMB = i
+        MCOUNT = ARR.count(i)
+
+print()
+
+print(f"{MNUMB} встречается чаще всего - {MCOUNT} раз")
