44a7905352176810ab47a158c3d84690d87ee365
Kirill <hotasice41@gmail.com>
2019-11-21 12:24:15 +0100

    задание 6

--- Lesson_3/6.py
+++ Lesson_3/6.py
@@ -3,3 +3,24 @@
 между минимальным и максимальным элементами.
 Сами минимальный и максимальный элементы в сумму не включать.
 """
+from random import random
+
+N = int(input("Укажите длину массива: "))
+arr = [0] * N
+for i in range(N):
+    arr[i] = int(random() * 100)
+    print(f"|{arr[i]:3d}|", end=' ')
+
+arr_min = arr.index(min(arr))
+arr_max = arr.index(max(arr))
+
+summ = 0
+
+if arr_min > arr_max:
+    for i in range(arr_max + 1, arr_min):
+        summ += arr[i]
+else:
+    for i in range(arr_min + 1, arr_max):
+        summ += arr[i]
+
+print(f'\nCумма элементов между минимальным {min(arr)} и максимальным элементами {max(arr)} равна {summ}.')
