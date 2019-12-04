21e75c88efd99fc6cf36c94201a91604f98b5d3b
Kirill <hotasice41@gmail.com>
2019-12-04 11:03:31 +0100

    1 задание сделано

--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,46 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+
+from pympler import asizeof
+from sys import getsizeof
+
+
+class Vehicle(object):
+    """docstring"""
+    
+    def __init__(self, color, doors, tires):
+        """Constructor"""
+        self.color = color
+        self.doors = doors
+        self.tires = tires
+
+    def brake(self):
+        """
+        Stop the car
+        """
+        return "Braking"
+
+    def drive(self):
+        """
+        Drive the car
+        """
+        return "I'm driving!"
+
+
+
+BC_OBJ = Vehicle('green', 5, 4)
+print(BC_OBJ.__dict__)
+print(asizeof.asizeof((BC_OBJ)))
+# if __name__ == "__main__":
+#     car = Vehicle("blue", 5, 4)
+#     print(car.color)
+#
+#     truck = Vehicle("red", 3, 6)
+#     print(truck.color)
+
+
+"""Реализация без слотов
+{'color': 'green', 'doors': 5, 'tires': 4}
+264
+"""
